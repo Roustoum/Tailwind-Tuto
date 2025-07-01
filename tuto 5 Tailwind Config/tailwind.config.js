b2346568d90ac2c42ddc4345d9b58b250710ad36
Roustoum Lamborghini <163304407+Roustoum@users.noreply.github.com>
2025-07-01 20:21:53 +0100

    Add files via upload

---
+++ tuto 5 Tailwind Config/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports =  {
+    theme: {
+      extend: {
+        colors: {
+            primary:'#FF6363',
+            secondary:{
+                100:'#E2E2D5',
+                200:'#888883',
+            }
+        },
+        fontSize: {  // fontSize au lieu de fontsize
+          mamoth: '10rem'
+        }
+      },
+    },
+    variants:{},
+    plugins: [],
+  }
