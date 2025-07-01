e27d5d740986dda6c7855cd212350134e1a82c70
Roustoum Lamborghini <163304407+Roustoum@users.noreply.github.com>
2025-07-01 20:26:44 +0100

    Add files via upload

---
+++ tuto 13 button/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports =  {
+    // content: ["./src/**/*.{html,js}","./public/index.html"],
+    theme: {
+      extend: {
+        colors: {
+            primary:'#FF6363',
+            secondary:{
+                100:'#E2E2D5',
+                200:'#888883',
+            }
+        },
+        fontSize: { 
+          //  rest:"100px"
+        },
+        fontFamily:{
+          tester:['test']
+        }
+      },
+    },
+    variants:{},
+    plugins: [],
+  }
